a4284518e86e41c1afca342c696104fbe6075253
MiyukiMizore <morgan.fusil2003@gmail.com>
2023-05-09 15:01:10 +0200

    premier ajout

---
+++ script.sh
@@ -0,0 +1,10 @@
+#INSTALL BREW
+
+curl -fsSL https://raw.githubusercontent.com/omimouni/42homebrew/master/install-goinfre.sh | zsh
+
+#INSTALL CMAKE
+
+brew install cmake
+
+#INSTALL GLFW
+brew install glfw
